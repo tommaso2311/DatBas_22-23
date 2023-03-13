75cd3877004373b7b40d5f3cdff59b4a94e00904
ariarru <arianna.arruzzoli@studio.unibo.it>
2023-03-13 12:51:12 +0100

    Fine form registrazione php
    scusate me l'ero scordata

--- register.php
+++ register.php
@@ -3,9 +3,13 @@ require 'connect.php';
 $conn = connectToDatabase();
 
 $email = $_POST['email'];
+$nome = $_POST['nome'];
+$cognome = $_POST['cognome'];
+$anno = $_POST['annoNascita'];
+$luogo = $_POST['luogoNascita'];
 
 try {
-    $insert = "INSERT INTO UTENTE (eMail) VALUES ('$email')";
+    $insert = "CALL RegistrazioneUtente('$email', '$nome', '$cognome','$anno','$luogo')";
     $conn->query($insert);
     echo "Aggiunto";
 } catch (PDOException $e) {
